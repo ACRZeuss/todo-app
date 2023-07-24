022ad600eaaa78c26f57c2ab011b80ec470d8dc9
Erhan <ep4963807@gmail.com>
2023-07-24 16:34:26 +0300

    first

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Todo.Models;
+using System.Data.SqlClient;
+using Todo.Models.ViewModels;
+
+namespace Todo.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connectionString = @"Server=(localdb)\MSSQLLocalDB;initial catalog=todo;integrated security=true";
+
+        public IActionResult Index()
+        {
+            var todoListViewModel = GetAllTodos();
+            return View(todoListViewModel);
+        }
+
+        [HttpGet]
+        public JsonResult PopulateForm(int id)
+        {
+            var todo = GetById(id);
+            return Json(todo);
+        }
+
+        internal TodoModel GetById(int id)
+        {
+            TodoModel todo = new();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = $"SELECT * FROM todo Where Id = '{id}'";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            todo.Id = reader.GetInt32(0);
+                            todo.Name = reader.GetString(1);
+                        }
+                        else
+                        {
+                            return todo;
+                        }
+                    }
+                }
+            }
+            return todo;
+        }
+
+        internal TodoViewModel GetAllTodos()
+        {
+            List<TodoModel> todoList = new();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = "SELECT * FROM todo";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                todoList.Add(new TodoModel
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1),
+                                });
+                            }
+                        }
+                        else
+                        {
+                            return new TodoViewModel
+                            {
+                                TodoList = todoList
+                            };
+                        }
+                    }
+                }
+            }
+            return new TodoViewModel
+            {
+                TodoList = todoList
+            };
+        }
+
+        public RedirectResult Insert(TodoModel todo)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("/");
+        }
+
+        public RedirectResult Update(TodoModel todo)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = $"UPDATE todo SET name = '{todo.Name}' WHERE Id = '{todo.Id}'";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = $"DELETE from todo WHERE Id = '{id}'";
+                    tableCmd.ExecuteNonQuery();
+                }
+            }
+            return Json(new { });
+        }
+    }
+}
